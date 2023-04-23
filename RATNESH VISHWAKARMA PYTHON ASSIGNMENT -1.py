fb62cb556a6437bceaa6350d6cc8b6f8c51b5592
Ratneshvishwakarma181025 <121814632+Ratneshvishwakarma181025@users.noreply.github.com>
2023-04-23 09:59:18 +0530

    Add files via upload

---
+++ RATNESH VISHWAKARMA PYTHON ASSIGNMENT -1.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### Que.- 1
+
+# In[1]:
+
+
+R = "hello world"   
+print(R)
+
+
+# #### Que.- 2
+
+# In[2]:
+
+
+A = "that is my first program in python"   
+t = print(A)
+T = "I will learn very deeply"
+print(t,T)
+
+
+# #### Que.- 3
+
+# In[3]:
+
+
+N = "Rohit"   
+print("Hello {}, would you like to learn some Python today?".format(N))
+
+
+# #### Que.- 4
+
+# In[4]:
+
+
+print('Albert Einstein once said, “A person who never made a mistake never tried anything new.”') 
+
+
+# #### Que.- 5
+
+# In[5]:
+
+
+Famous_person = "Albert Einstein"
+
+print(Famous_person,"once said “A person who never made a mistake never tried anything new.”")
+
+
+# #### Que.- 6 
+
+# In[6]:
+
+
+#addition-
+a = 2
+b = 3
+c = 3
+D = a+b+c
+print(D)
+
+
+# In[7]:
+
+
+# subtraction-
+a = 68
+b = 50
+c = 10
+D = a-b-c
+print(D)
+
+
+# In[8]:
+
+
+# multiplication-
+a = 2
+b = 2
+c = 2
+D = (a+b)*c
+print(D)
+
+
+# In[9]:
+
+
+# division-
+a = 32
+b = 2
+c = 8
+D = (a*b)/c
+print(D)
+
+
+# #### Que.- 7
+
+# In[10]:
+
+
+r = 25
+print("My favourite number is {}".format(r))
+
+
+# #### Que.- 8 
+
+# In[14]:
+
+
+# Ratnesh vishwakarma - 16 apr (this program is represent my faburate number)
+r = 25
+print("My favourite number is {}".format(r))
+
+
+# In[15]:
+
+
+# Ratnesh vishwakarma - 16 apr ((this program is represent adding of three num.) 
+a = 2
+b = 3
+c = 3
+D = a+b+c
+print(D)
+
+
+# #### Que.- 9
+
+# In[11]:
+
+
+names = ['Rohit', 'Diwakar', 'Sachin','Shivnarayan']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+
+# #### Que.- 10
+
+# In[12]:
+
+
+names = ['Rohit', 'Diwakar', 'Sachin','Shivnarayan']
+print("very simple and innocent person {}".format(names[0]))
+print("very simple and innocent person {}".format(names[1]))
+print("very simple and innocent person {}".format(names[2]))
+print("very simple and innocent person {}".format(names[3]))
+
+
+# #### Que.- 11 
+
+# In[13]:
+
+
+favorite_transportation = ['motorcycle','car']
+print("I would like to own a pulsar {}".format(favorite_transportation[0]))
+print("I like innova {}".format(favorite_transportation[1]))
+
+
+# # complete-
